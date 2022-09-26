7e0691aa6afbb84c813f9eac1854a5b76f563ca1
MehraannAbbas <githubcodee@gmail.com>
2022-09-26 15:45:09 +0500

    file-system-module

--- first_commit.js
+++ first_commit.js
@@ -1,3 +1,9 @@
-function getName() {
-    console.log('name')
-}
+const fs = require('fs');
+
+//const files = fs.readdirSync('./');
+//console.log(files);
+
+fs.readdir('./', function (err, files) {
+    if (err) console.log('Error',err);
+    else console.log('Result',files);
+});
