083c110763dcf56240860ffbfe15be84160f087e
MehraannAbbas <githubcodee@gmail.com>
2022-09-27 20:37:21 +0500

    useing a package

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var _ = require('underscore');
 // core module
 // file or folder
 // node_module
-var result = _.contains([1, 2, 3], 2);
+var result = _.contains([1, 2, 3, 4,5 ,6 ,7, 8], 0);
 console.log(result);
