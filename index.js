7ad664e5c0fa705589163845db0407c5b44511cc
MehraannAbbas <githubcodee@gmail.com>
2022-10-03 18:21:07 +0500

    handeling put request

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
+
+app.use(express.json());
 
 const courses = [
     { id: 1, name: 'course1' },
@@ -16,6 +19,12 @@ app.get('/api/courses', (req, res) =>{
 });
 
 app.post('/api/courses', (req, res) => {
+const { error } = validateCourse(req.body);
+    if (error) {
+     res.status(400).send(error.details[0].message);
+     return;
+ }
+
     const course = {
         id: courses.length + 1,
         name: req.body.name
@@ -23,6 +32,30 @@ app.post('/api/courses', (req, res) => {
     courses.push(course);
     res.send(course);
 });
+
+app.put('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+   if(!course) res.status(404).send('The course with given id was not found');
+
+   const { error } = validateCourse(req.body);
+   if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+}
+
+course.name = req.body.name;
+res.send(course);
+});
+
+function validateCourse(course) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+    
+    return  schema.validate(course);
+     
+}
+
 
 app.get('/api/courses/:id', (req, res) =>{
    const course = courses.find(c => c.id === parseInt(req.params.id));
