848f9f6ff10e8a89735e5998264692238d5a2a38
MehraannAbbas <githubcodee@gmail.com>
2022-10-19 17:40:10 +0500

    callback

--- index.js
+++ index.js
@@ -28,7 +28,25 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Liatening on port ${port}...`));*/
 
 console.log('Before');
-setTimeout(() => {
-    console.log('Reading a user from a database');
-}, 2000);
+getUser(1, (user) => {
+
+    getRepositories(user.gitHubUsername, (repos) => {
+        console.log('Repos', repos);
+    });
+
+});
 console.log('After');
+
+function getUser(id, callback) {
+    setTimeout(() => {
+        console.log('Reading a user from a database.......');
+        callback({ id: id, gitHubUsername: 'mosh'});
+    }, 2000);
+}
+
+function getRepositories(username, callback) {
+    setTimeout(() => {
+        console.log('Calling GitHub API.....');
+        callback(['repo1', 'repo2', 'repo3']);
+    }, 2000);
+}
